81670b96f876965151419458868fabe24a1a65e7
afattobene <adila.fattobene@ifood.com.br>
2021-06-17 14:34:27 -0300

    feat: create createFilm endpoint

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
+app.use(express.json());
 
 app.listen(port, () => console.log("Service runing at port ", port));
 
@@ -32,4 +33,17 @@ app.get("/api/films/:id", (req, res) => {
   }
 
   return res.status(404).send("Filme não existe.");
+});
+
+app.post("/api/films", (req, res) => {
+  const film = {
+    id: films.length + 1,
+    name: req.body.name,
+    director: req.body.director,
+    link: req.body.link,
+  };
+
+  films.push(film);
+
+  return res.status(201).send(films);
 });
